a09b0b64f873bc07535116d3b7f3b6bb35e8a475
cshin <josa0005@naver.com>
2024-06-24 11:07:00 +0900

    [2024-06-24]신철 13문제 업로드

---
+++ Problem/week-13/신철/basic_group_anagram.kt
@@ -0,0 +1,34 @@
+/*
+
+키워드 - 정렬, 그룹 애너그램
+https://leetcode.com/problems/group-anagrams/description/
+
+49. Group Anagrams
+문자열 배열 strs가 주어지면 철자 바꾸기를 함께 그룹화합니다. 어떤 순서로든 답변을 반환할 수 있습니다.
+Anagram은 일반적으로 모든 원래 문자를 정확히 한 번 사용하여 다른 단어나 구문의 문자를 재배열하여 형성된 단어나 구문입니다.
+
+Example 1:
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Example 2:
+Input: strs = [""]
+Output: [[""]]
+
+Example 3:
+Input: strs = ["a"]
+Output: [["a"]]
+ 
+Constraints:
+1 <= strs.length <= 104
+0 <= strs[i].length <= 100
+strs[i] consists of lowercase English letters.
+ */
+
+ /**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+
+ //그룹 애나그램 관련
+
